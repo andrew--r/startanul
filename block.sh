9286ec1baffc41d6187d5e4182fe7228e8a856ec
Andrew Romanov <scorpion21.97@gmail.com>
2016-02-12 14:11:15 +0600

    Переделал структуру файлов стилей, привёл код к единообразному виду, переделал сброс стилей, добавил файл для утилитарных классов

--- block.sh
+++ block.sh
@@ -3,7 +3,7 @@ mkdir app/blocks/$1
 touch app/blocks/$1/$1.styl
 touch app/blocks/$1/$1.jade
 
-echo -e "@import \"../blocks/$1/*\"" >> app/styles/main.styl
+echo -e "@import '../blocks/$1/*'" >> app/styles/main.styl
 echo -e "mixin $1()\n\t.$1&attributes(attributes)" >> app/blocks/$1/$1.jade
 
 echo "Блок $1 создан"
