c21690fc607afbbed2bf151fe69216f7c1dddfb6
Andrew Romanov <scorpion21.97@gmail.com>
2016-03-02 15:52:35 +0600

    Упростил перенос статичных файлов в папку dist/

--- gulp/tasks/static.js
+++ gulp/tasks/static.js
@@ -4,5 +4,5 @@ import paths from '../paths';
 gulp.task('static', () => {
 	return gulp
 		.src(`${paths.src.static}/**/*`)
-		.pipe(gulp.dest(paths.dist.static));
+		.pipe(gulp.dest(paths.baseDist));
 });
