3842195ec8983e59d6b7a8828b18d553c11dcd8d
Andrew Romanov <scorpion21.97@gmail.com>
2016-09-30 11:32:20 +0300

    import block.sh script

--- block.sh
+++ block.sh
@@ -4,6 +4,7 @@ touch app/blocks/$1/$1.styl
 touch app/blocks/$1/$1.jade
 
 echo -e "@import '../blocks/$1/*'" >> app/styles/main.styl
-echo -e "mixin $1()\n\t.$1&attributes(attributes)" >> app/blocks/$1/$1.jade
+echo -e "mixin $1()\n\t+b.$1&attributes(attributes)" >> app/blocks/$1/$1.jade
+echo -e ".$1" >> app/blocks/$1/$1.styl
 
 echo "Блок $1 создан"
